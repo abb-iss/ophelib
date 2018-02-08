31cf7de5d54535c1deab99f30d86c66d537e7361
Ognjen Vukovic <ognjen.vukovic@ch.abb.com>
2018-02-08 19:36:26 +0100

    Move the demo program in its own folder, create DEMO.md

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 [ -f Makefile ] && make clean
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile include/ophelib/wire/*_generated.h
+rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake Makefile include/ophelib/wire/*_generated.h demo/hello_world demo/rand.txt bin/rand.txt
